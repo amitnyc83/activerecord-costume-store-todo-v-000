5aeafc799cc9ed5c8fae3d9bb3fc6502f840c1e2
Amit Singh <amit_nyc@live.com>
2018-09-02 19:28:30 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |c|
       c.text :name
       c.text :location
-      c.text :themes
+      c.text :theme
       c.integer :price
       c.boolean :family_friendly
       c.datetime :opening_date
